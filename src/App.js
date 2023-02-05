6cce56d635f25e5bda217dfa59c7d5c7658872ad
Paolo Federle <federlepaolo95@gmail.com>
2023-02-05 13:12:04 +0100

    corrected remove dice behaviour and changing input while having a result

--- src/App.js
+++ src/App.js
@@ -5,24 +5,41 @@ function RollDice() {
   const [diceOptions, setDiceOptions] = useState([{ numDice: 1, numSides: 6 }]);
   const [results, setResults] = useState([]);
   const [hideRolls, setHideRolls] = useState(false);
+  const [diceOptionsCopy, setDiceOptionsCopy] = useState([]);
+  const [resultsShouldUpdate, setResultsShouldUpdate] = useState(true);
+
 
   const roll = () => {
     let newResults = [];
-    diceOptions.forEach(({ numDice, numSides }) => {
+    const diceOptionsCopy = JSON.parse(JSON.stringify(diceOptions));
+    setDiceOptionsCopy(diceOptionsCopy)
+    diceOptionsCopy.forEach(({ numDice, numSides }) => {
+      let newNumDice = numDice
+      let newNumSides = numSides
       let result = [];
       let total = 0;
-      for (let i = 0; i < numDice; i++) {
-        let roll = Math.floor(Math.random() * numSides) + 1;
+      for (let i = 0; i < newNumDice; i++) {
+        let roll = Math.floor(Math.random() * newNumSides) + 1;
         result.push(roll);
         total += roll;
+        console.log('total ', total)
       }
       newResults.push({ result, total });
     });
     setResults(newResults);
+    console.log('newResults ', newResults)
   };
 
-  const addDiceOption = () => {
-    setDiceOptions([...diceOptions, { numDice: 1, numSides: 6 }]);
+  const addDiceOption = () => setDiceOptions([...diceOptions, { numDice: 1, numSides: 6 }]);
+
+  const handleRollDice = () => {
+    let updatedOptions = [...diceOptions];
+    updatedOptions.forEach((diceOption, index) => {
+      diceOption.numDice = parseInt(diceOption.numDice, 10);
+      diceOption.numSides = parseInt(diceOption.numSides, 10);
+    });
+    setDiceOptions(updatedOptions);
+    roll();
   };
 
   const updateDiceOption = (index, key, value) => {
@@ -31,37 +48,46 @@ function RollDice() {
     setDiceOptions(updatedOptions);
   };
 
+  const handleRemoveDie = (index) => {
+    setDiceOptions(diceOptions.filter((_, i) => i !== index));
+  };
+  
+
   return (
     <div className="background">
       <div className="main">
         {diceOptions.map((diceOption, index) => (
           <div key={index} className="dice-input">
-            
+
             <input
               type="number"
               value={diceOption.numDice}
               onChange={(e) => updateDiceOption(index, "numDice", e.target.value)}
             />
             <label>dice</label>
-            
+
             <input
               type="number"
               value={diceOption.numSides}
               onChange={(e) => updateDiceOption(index, "numSides", e.target.value)}
             />
             <label>sided</label>
+
+            {index !== 0 && (
+              <button className="remove-dice-button" onClick={() => handleRemoveDie(index)}>Remove</button>
+            )}
           </div>
         ))}
         <button className="add-dice-button" onClick={addDiceOption}>Add Dice</button>
         <div className="dice-result">
           <div>
-            {!hideRolls && results.map(({ result, total }, index) => (
+          {!hideRolls && results.map(({ result, total }, index) => (
               <React.Fragment key={index}>
                 <span className="result-set">
                   ({result.map((roll, rollIndex) => (
                     <React.Fragment key={rollIndex}>
                       <span
-                        className={`result ${roll == diceOptions[index].numSides ? "highlight-green" : roll === 1 ? "highlight-red" : ""}`}
+                        className={`result ${ diceOptionsCopy[index].numSides && roll == diceOptionsCopy[index].numSides ? "highlight-green" : roll === 1 ? "highlight-red" : ""}`}
                       >
                         {roll}
                       </span>
@@ -74,7 +100,9 @@ function RollDice() {
             ))}
             = <span className="roll-result">{results.reduce((total, { total: rollTotal }) => total + rollTotal, 0)}</span>
           </div>
-          <button className="roll-dice-button" onClick={roll}>Roll Dice</button>
+          <button className="roll-dice-button" onClick={handleRollDice}>Roll Dice</button>
+
+
         </div>
         <div>
           <input type="checkbox" checked={hideRolls} onChange={() => setHideRolls(!hideRolls)} />
